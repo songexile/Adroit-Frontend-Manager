4a977a6e5450fb43f8e22f9cf5f08ee356982cc6
munishk686 <munishk686@gmail.com>
2024-02-08 15:40:56 +1300

    added body component

--- src/app/pages/page.tsx
+++ src/app/pages/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../components/Header'; // Import the Header component
+import Body from '../components/Body'; // Import the Body component
 
 const HomePage: React.FC = () => {
   return (
     <div>
       {/* Render the Header component */}
       <Header /> 
-
+      <Body />
     </div>
   );
 };
