2b9c4f000c6050e011075b630060e6a4e1cc5960
munishk686 <munishk686@gmail.com>
2024-02-08 15:18:35 +1300

    Added Header part of website

---
+++ src/app/pages/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header'; // Import the Header component
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      {/* Render the Header component */}
+      <Header /> 
+
+    </div>
+  );
+};
+
+export default HomePage;
