41077e0496a971c667d6378167032e9350af5314
munishk686 <munishk686@gmail.com>
2024-02-22 14:16:26 +1300

    table updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,44 @@
+import React, { useEffect } from "react";
 import Header from "../components/Header";
 import { DataTable } from "./iotTable/data-table";
 import { Payment, columns } from "./iotTable/columns";
 
- 
+let data = null;
+
 async function getData() {
-  const res = await fetch('https://newiotdata.s3.ap-southeast-2.amazonaws.com/SensorData.json')
-  
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
- 
+  const res = await fetch('https://eq1n7rs483.execute-api.ap-southeast-2.amazonaws.com/Prod/hello');
+
   if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
+    throw new Error('Failed to fetch data');
   }
- 
-  return res.json()
+
+  return res.json();
 }
- 
-export default async function Page() {
-  const data = await getData()
-  console.log('Fetched data:', data);
- 
+
+export default function Page() {
+  
+  async function fetchData() {
+    try {
+      data = await getData();
+    } catch (error) {
+      console.error('Failed to fetch data:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log('Fetched data:', data);
+    }, 10000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className=" ">
+    <div className="">
       <Header />
       <DataTable columns={columns} data={data} />
     </div>
