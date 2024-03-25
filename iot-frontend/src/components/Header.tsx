143a802995852eb85d0c8f336d52db0529a422e7
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2024-03-25 22:54:46 +1300

    Added and Refactor Header component for search functionality

--- iot-frontend/src/components/Header.tsx
+++ iot-frontend/src/components/Header.tsx
@@ -1,63 +1,66 @@
 import Link from 'next/link'
-import React from 'react'
 
-const Header = ({ fetchDataAndUpdate }: { fetchDataAndUpdate?: () => Promise<void> }) => {
+const Header = ({
+  fetchDataAndUpdate,
+  searchById,
+  setSearchById,
+  searchByClientName,
+  setSearchByClientName,
+}: HeaderProps) => {
   const handleFetchAndUpdate = async () => {
-    if (fetchDataAndUpdate !== undefined) {
+    if (fetchDataAndUpdate) {
       await fetchDataAndUpdate()
     }
   }
 
   return (
-    <main>
-      <>
-        {/* Top Blue Header */}
-        <div className="bg-blue-800 text-white py-2 px-4">
-          <div className="container mx-auto flex justify-between items-center">
-            <Link href={'/'} className="font-bold">
-              Adroit
-            </Link>
-          </div>
+    <>
+      {/* Top Blue Header */}
+      <div className="bg-blue-800 text-white py-2 px-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <Link href={'/'} className="font-bold">
+            Adroit
+          </Link>
         </div>
-
-        {/* Light Grey Header */}
-        <div className="bg-gray-200 py-2 px-4">
-          <div className="container mx-auto md:flex items-center justify-between">
-            {/* Search By ID */}
-            <div className="flex items-center space-x-2">
+      </div>
+      <div className="bg-gray-200 py-2 px-4">
+        <div className="container mx-auto md:flex items-center justify-between">
+          {/* Search By ID */}
+          <div className="flex items-center space-x-2">
+            {setSearchById && (
               <input
                 className="px-3 py-1 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
                 type="text"
                 placeholder="Search by ID"
-                //  value={searchById}
-                //  onChange={(e) => setSearchById(e.target.value)}
+                value={searchById}
+                onChange={(e) => setSearchById(e.target.value)}
               />
-            </div>
-
-            {/* Search By Location */}
-            <div className="flex items-center space-x-2 mt-3 md:mt-0">
+            )}
+          </div>
+          {/* Search By ClientName */}
+          <div className="flex items-center space-x-2 mt-3 md:mt-0">
+            {setSearchByClientName && (
               <input
                 className="px-3 py-1 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
                 type="text"
-                placeholder="Search by Location"
-                //   value={searchByLocation}
-                //  onChange={(e) => setSearchByLocation(e.target.value)}
+                placeholder="Search by Client Name"
+                value={searchByClientName}
+                onChange={(e) => setSearchByClientName(e.target.value)}
               />
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded focus:outline-none">
-                Search
-              </button>
-            </div>
-
+            )}
+          </div>
+          {/* Fetch New Data Button */}
+          {fetchDataAndUpdate && (
             <button
               onClick={handleFetchAndUpdate}
-              className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+              className="bg-blue-500 text-white font-bold py-2 px-4 rounded md:mt-0 mt-3"
             >
               Fetch New Data
             </button>
-          </div>
+          )}
         </div>
-      </>
-    </main>
+      </div>
+    </>
   )
 }
 
