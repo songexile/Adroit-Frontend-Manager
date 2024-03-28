578fb3131a9e37d61aeffb172a43f147eac88f30
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2024-03-28 18:13:25 +1300

    Create ticket page component

---
+++ iot-frontend/src/pages/create-ticket/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
+const createTicket = () => {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <p>Create Ticket Page</p>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default createTicket
