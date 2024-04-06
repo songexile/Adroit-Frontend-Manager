6880d0975239ca6e557101ee827613dca6a78591
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2024-04-06 18:57:56 +1300

    Add session check and breadcrumb to DeviceID and CreateTicket pages

--- iot-frontend/src/pages/device-info/[deviceID].tsx
+++ iot-frontend/src/pages/device-info/[deviceID].tsx
@@ -5,6 +5,8 @@ import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import Breadcrumb from '@/components/Breadcrumb'
 import Link from 'next/link'
+import LoginScreen from '../login'
+import { useSession } from 'next-auth/react'
 
 function fetchDeviceId() {
   // Fetches deviceId from Url
@@ -14,7 +16,8 @@ function fetchDeviceId() {
   return deviceId
 }
 
-function Page(data: any) {
+function DeviceID(data: any) {
+  const { data: session } = useSession()
   const deviceId = fetchDeviceId()
   const filteredData = flattenNestedData(data, deviceId)
   const deviceData = filteredData[0]
@@ -26,78 +29,82 @@ function Page(data: any) {
     { name: `Device ${deviceData?.device_id}`, path: `/device-info/${deviceData?.device_id}` }, // Adjusted path to include device_id
   ]
 
-  return (
-    <>
-      <Header />
-      <Breadcrumb breadcrumbs={breadcrumbs} />
+  if (session) {
+    return (
+      <>
+        <Header />
+        <Breadcrumb breadcrumbs={breadcrumbs} />
 
-      <div className="container min-h-screen max-w-3xl mx-auto p-6 py-5 border rounded-lg mt-8 shadow-md">
-        {/* Device Info */}
-        <div className="mx-auto">
-          <div className="text-xl font-bold mb-2">Device Info</div>
-          <div className="mb-2">
-            <span className="font-semibold">Device ID:</span> {deviceData?.device_id}
+        <div className="container min-h-screen max-w-3xl mx-auto p-6 py-5 mb-5 border rounded-lg mt-8 shadow-md">
+          {/* Device Info */}
+          <div className="mx-auto">
+            <div className="text-xl font-bold mb-2">Device Info</div>
+            <div className="mb-2">
+              <span className="font-semibold">Device ID:</span> {deviceData?.device_id}
+            </div>
+            <div className="mb-2">
+              <span className="font-semibold">Device Key:</span> {deviceData?.device_key}
+            </div>
+            <div className="mb-2">
+              <span className="font-semibold">Client Name:</span> {deviceData?.client_name}
+            </div>
+            <div className="mb-2">
+              <span className="font-semibold">Last Online:</span>{' '}
+              {typeof deviceData?.last_online === 'string'
+                ? deviceData.last_online
+                : deviceData?.last_online?.value || 'N/A'}
+            </div>
+            <div className="mb-2">
+              <span className="font-semibold">Last ticket created:</span> Never
+            </div>
           </div>
-          <div className="mb-2">
-            <span className="font-semibold">Device Key:</span> {deviceData?.device_key}
-          </div>
-          <div className="mb-2">
-            <span className="font-semibold">Client Name:</span> {deviceData?.client_name}
-          </div>
-          <div className="mb-2">
-            <span className="font-semibold">Last Online:</span>{' '}
-            {typeof deviceData?.last_online === 'string'
-              ? deviceData.last_online
-              : deviceData?.last_online?.value || 'N/A'}
-          </div>
-          <div className="mb-2">
-            <span className="font-semibold">Last ticket created:</span> Never
-          </div>
-        </div>
 
-        {/* Status */}
-        <div className="flex items-center py-5">
-          <div className="text-xl font-bold mr-4">
-            Scan:
-            <button className="bg-green-500 text-white font-bold py-2 px-4 rounded ml-1">
-              ONLINE
-            </button>
-          </div>
-          <div className="text-xl font-bold mr-4">
-            Battery:
-            <button className="bg-red-500 text-white font-bold py-2 px-4 rounded ml-1">
-              OFFLINE
-            </button>
-          </div>
-          <div className="text-xl font-bold mr-4">
-            Insitu:
-            <button className="bg-red-500 text-white font-bold py-2 px-4 rounded ml-1">
-              ERROR
-            </button>
-          </div>
-        </div>
-
-        {/* Create Ticket */}
-        <div className="flex flex-col py-5">
-          <div className="flex justify-center">
-            <Link href={`/create-ticket/${deviceData?.device_id}`}>
-              <button className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 mb-4 text-xl">
-                Create Ticket
+          {/* Status */}
+          <div className="flex items-center py-5">
+            <div className="text-xl font-bold mr-4">
+              Scan:
+              <button className="bg-green-500 text-white font-bold py-2 px-4 rounded ml-1">
+                ONLINE
               </button>
-            </Link>
+            </div>
+            <div className="text-xl font-bold mr-4">
+              Battery:
+              <button className="bg-red-500 text-white font-bold py-2 px-4 rounded ml-1">
+                OFFLINE
+              </button>
+            </div>
+            <div className="text-xl font-bold mr-4">
+              Insitu:
+              <button className="bg-red-500 text-white font-bold py-2 px-4 rounded ml-1">
+                ERROR
+              </button>
+            </div>
           </div>
-          <p className="text-center">No ticket currently active.</p>
-        </div>
 
-        {/* Metrics */}
-        <div className="mb-4">
-          <p className="text-xl font-semibold">Metrics:</p>
-          {deviceData && renderMetrics(deviceData)}
+          {/* Create Ticket */}
+          <div className="flex flex-col py-5">
+            <div className="flex justify-center">
+              <Link href={`/create-ticket/${deviceData?.device_id}`}>
+                <button className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 mb-4 text-xl">
+                  Create Ticket
+                </button>
+              </Link>
+            </div>
+            <p className="text-center">No ticket currently active.</p>
+          </div>
+
+          {/* Metrics */}
+          <div className="mb-4">
+            <p className="text-xl font-semibold">Metrics:</p>
+            {deviceData && renderMetrics(deviceData)}
+          </div>
         </div>
-      </div>
-      <Footer />
-    </>
-  )
+        <Footer />
+      </>
+    )
+  } else {
+    return <LoginScreen />
+  }
 }
 
 /**
@@ -140,4 +147,4 @@ const renderMetrics = (deviceData: DynamicMetricData | null): JSX.Element | null
   )
 }
 
-export default Page
+export default DeviceID
