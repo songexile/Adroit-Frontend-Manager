6efff4e827881652fbcddc63bc17862a8fa848d4
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2024-04-07 03:07:03 +1200

    Profile page attemp

--- iot-frontend/src/pages/profile/index.tsx
+++ iot-frontend/src/pages/profile/index.tsx
@@ -5,6 +5,15 @@ import { useSession } from 'next-auth/react'
 import { useEffect, useState } from 'react'
 import { CustomUser } from '@/types'
 import { showToast } from '@/components/Toast'
+import { updateUserAttribute } from 'aws-amplify/auth'
+import { Amplify } from "aws-amplify";
+import awsConfig from '@/utils/AWS-config';
+
+Amplify.configure({
+  Auth: {
+    ...awsConfig.Auth,
+  },
+});
 
 const Profile = () => {
   const { data: session } = useSession()
@@ -15,25 +24,59 @@ const Profile = () => {
   // Extract user data if session exists
   useEffect(() => {
     if (session) {
-      // console.log(session);
       const { given_name, family_name, email } = session.user as CustomUser
-      setGivenName(given_name)
-      setFamilyName(family_name)
-      setEmail(email)
+      setGivenName(given_name || '')
+      setFamilyName(family_name || '')
+      setEmail(email || '')
     }
   }, [session])
+
+  const handleUpdateAttribute = async (attributeKey: string, value: string) => {
+    try {
+      const output = await updateUserAttribute({
+        userAttribute: {
+          attributeKey,
+          value,
+        },
+      })
+      handleUpdateAttributeNextSteps(output)
+    } catch (error: any) {
+      showToast({ message: 'Error updating user attribute: ' + error.message, type: 'error' })
+    }
+  }
+
+  const handleUpdateAttributeNextSteps = (output: any) => {
+    const { nextStep } = output
+
+    switch (nextStep.updateAttributeStep) {
+      case 'CONFIRM_ATTRIBUTE_WITH_CODE':
+        const codeDeliveryDetails = nextStep.codeDeliveryDetails
+        showToast({
+          message: `Confirmation code was sent to ${codeDeliveryDetails?.deliveryMedium}.`,
+          type: 'info',
+        })
+        // Collect the confirmation code from the user and pass to confirmUserAttribute.
+        break
+      case 'DONE':
+        showToast({ message: 'Attribute was successfully updated.', type: 'success' })
+        break
+      default:
+        showToast({ message: 'Unknown next step.', type: 'error' })
+        break
+    }
+  }
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
     try {
-      showToast({ message: 'User profile updated successfully!', type: 'success' })
-    } catch (error) {
-      showToast({ message: 'Error updating user profile: ' + error, type: 'error' })
-
+      await handleUpdateAttribute('given_name', givenName)
+      await handleUpdateAttribute('family_name', familyName)
+      await handleUpdateAttribute('email', email)
+    } catch (error: any) {
+      showToast({ message: 'Error updating user attributes: ' + error.message, type: 'error' })
     }
   }
-
   if (session) {
     return (
       <>
