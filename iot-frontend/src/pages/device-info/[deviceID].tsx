199ba8939159a1b2385a36fd1ec811254d2c5712
songexile <therunningbeta@gmail.com>
2024-04-07 14:17:39 +1200

    Changed device id

--- iot-frontend/src/pages/device-info/[deviceID].tsx
+++ iot-frontend/src/pages/device-info/[deviceID].tsx
@@ -37,74 +37,73 @@ function DeviceID(data: any) {
         <Breadcrumb breadcrumbs={breadcrumbs} />
 
         <div className="flex-grow flex flex-col container mx-auto p-6 py-5">
-        <div className="max-w-4xl mx-auto bg-white rounded-lg shadow-lg overflow-hidden animate-slide-in border-2 border-blue-500">
-          <div className="p-8">
-            <div className="flex justify-between items-center mb-8">
-              <h2 className="text-2xl font-bold text-gray-800">Device Info</h2>
-              <Link href={`/create-ticket/${deviceData?.device_id}`}>
-                <button className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 transition duration-300 shadow-md">
-                  Create Ticket
-                </button>
-              </Link>
-            </div>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-              <div className="bg-white p-6 rounded-lg shadow-md border-2 border-blue-500">
-                <div className="mb-4">
-                  <span className="font-semibold text-gray-600">Device ID:</span>{' '}
-                  {deviceData?.device_id}
-                </div>
-                <div className="mb-4">
-                  <span className="font-semibold text-gray-600">Device Key:</span>{' '}
-                  {deviceData?.device_key}
-                </div>
-                <div className="mb-4">
-                  <span className="font-semibold text-gray-600">Client Name:</span>{' '}
-                  {deviceData?.client_name}
-                </div>
-                <div className="mb-4">
-                  <span className="font-semibold text-gray-600">Last Online:</span>{' '}
-                  {typeof deviceData?.last_online === 'string'
-                    ? deviceData.last_online
-                    : deviceData?.last_online?.value || 'N/A'}
-                </div>
-                <div className="mb-4">
-                  <span className="font-semibold text-gray-600">Last ticket created:</span> Never
-                </div>
+          <div className="max-w-4xl mx-auto bg-white rounded-lg shadow-lg overflow-hidden animate-slide-in border-2 border-blue-500">
+            <div className="p-8">
+              <div className="flex justify-between items-center mb-8">
+                <h2 className="text-2xl font-bold text-gray-800">Device Info</h2>
+                <Link href={`/create-ticket/${deviceData?.device_id}`}>
+                  <button className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 transition duration-300 shadow-md">
+                    Create Ticket
+                  </button>
+                </Link>
               </div>
 
-              <div className="bg-white p-6 rounded-lg shadow-md border-2 border-blue-500">
-                <h3 className="text-xl font-bold mb-4 text-gray-800">Status</h3>
-                <div className="flex flex-col space-y-2">
-                  <div className="flex items-center">
-                    <span className="font-semibold text-gray-600 mr-2">Scan:</span>
-                    <span className="bg-green-100 text-green-800 px-3 py-1 rounded-full font-semibold">
-                      ONLINE
-                    </span>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                <div className="bg-white p-6 rounded-lg shadow-md border-2 border-blue-500">
+                  <div className="mb-4">
+                    <span className="font-semibold text-gray-600">Device ID:</span>{' '}
+                    {deviceData?.device_id}
                   </div>
-                  <div className="flex items-center">
-                    <span className="font-semibold text-gray-600 mr-2">Battery:</span>
-                    <span className="bg-red-100 text-red-800 px-3 py-1 rounded-full font-semibold">
-                      OFFLINE
-                    </span>
+                  <div className="mb-4">
+                    <span className="font-semibold text-gray-600">Device Key:</span>{' '}
+                    {deviceData?.device_key}
                   </div>
-                  <div className="flex items-center">
-                    <span className="font-semibold text-gray-600 mr-2">Insitu:</span>
-                    <span className="bg-red-100 text-red-800 px-3 py-1 rounded-full font-semibold">
-                      ERROR
-                    </span>
+                  <div className="mb-4">
+                    <span className="font-semibold text-gray-600">Client Name:</span>{' '}
+                    {deviceData?.client_name}
+                  </div>
+                  <div className="mb-4">
+                    <span className="font-semibold text-gray-600">Last Online:</span>{' '}
+                    {typeof deviceData?.last_online === 'string'
+                      ? deviceData.last_online
+                      : deviceData?.last_online?.value || 'N/A'}
+                  </div>
+                  <div className="mb-4">
+                    <span className="font-semibold text-gray-600">Last ticket created:</span> Never
+                  </div>
+                </div>
+
+                <div className="bg-white p-6 rounded-lg shadow-md border-2 border-blue-500">
+                  <h3 className="text-xl font-bold mb-4 text-gray-800">Status</h3>
+                  <div className="flex flex-col space-y-2">
+                    <div className="flex items-center">
+                      <span className="font-semibold text-gray-600 mr-2">Scan:</span>
+                      <span className="bg-green-100 text-green-800 px-3 py-1 rounded-full font-semibold">
+                        ONLINE
+                      </span>
+                    </div>
+                    <div className="flex items-center">
+                      <span className="font-semibold text-gray-600 mr-2">Battery:</span>
+                      <span className="bg-red-100 text-red-800 px-3 py-1 rounded-full font-semibold">
+                        OFFLINE
+                      </span>
+                    </div>
+                    <div className="flex items-center">
+                      <span className="font-semibold text-gray-600 mr-2">Insitu:</span>
+                      <span className="bg-red-100 text-red-800 px-3 py-1 rounded-full font-semibold">
+                        ERROR
+                      </span>
+                    </div>
+                  </div>
+                  <div className="max-w-4xl mx-auto bg-white rounded-lg shadow-lg p-8 mt-8 border-2 border-blue-500">
+                    <h2 className="text-2xl font-bold mb-4 text-gray-800">Metrics:</h2>
+                    {deviceData && renderMetrics(deviceData)}
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-
-        <div className="max-w-4xl mx-auto bg-white rounded-lg shadow-lg p-8 mt-8 border-2 border-blue-500">
-          <h2 className="text-2xl font-bold mb-4 text-gray-800">Metrics:</h2>
-          {deviceData && renderMetrics(deviceData)}
-        </div>
-      </div>
         <Footer />
       </>
     )
