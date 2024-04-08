79fc5f64c56810ba9313e851cd13a02be7d07a3c
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2024-04-08 16:44:47 +1200

    Fix Mobile Responsiveness: Update table styles for better overflow handling

--- iot-frontend/src/components/ui/table.tsx
+++ iot-frontend/src/components/ui/table.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/libs/utils'
 
 const Table = React.forwardRef<HTMLTableElement, React.HTMLAttributes<HTMLTableElement>>(
   ({ className, ...props }, ref) => (
-    <div className="relative w-full  ">
+    <div className="relative w-full overflow-auto">
       <table ref={ref} className={cn('w-full caption-bottom text-sm', className)} {...props} />
     </div>
   )
@@ -15,7 +15,7 @@ const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn('[&_tr]:border-b  ', className)} {...props} />
+  <thead ref={ref} className={cn('[&_tr]:border-b', className)} {...props} />
 ))
 TableHeader.displayName = 'TableHeader'
 
