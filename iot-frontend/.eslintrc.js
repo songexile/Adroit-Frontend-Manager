94b4fd2bb312497590bee437e7c80a914778a764
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2024-04-15 11:11:30 +1200

    Revert Linting Config

--- iot-frontend/.eslintrc.js
+++ iot-frontend/.eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   // Configuration for JavaScript files
   extends: [
     'airbnb-base',
-    'airbnb-typescript',
     'next/core-web-vitals', // Needed to avoid warning in next.js build: 'The Next.js plugin was not detected in your ESLint configuration'
     'plugin:prettier/recommended',
   ],
