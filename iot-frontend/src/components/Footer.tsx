e74a3af374f607f84bd27bc0361bc6bc1bd9c834
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2024-05-05 16:10:48 +1200

    Bump Version

--- iot-frontend/src/components/Footer.tsx
+++ iot-frontend/src/components/Footer.tsx
@@ -4,7 +4,7 @@ const establishedYear = 2015
 
 const Footer = () => {
   return (
-    <footer className="bg-blue-800 text-white py-2 px-4">
+    <footer className="bg-gradient-to-b  from-cyan-600 to-blue-500 text-white py-2 px-4">
       <div className="container mx-auto flex items-center justify-center">
         <p className="text-sm text-center">
           Copyright © Adroit {establishedYear} - {currentYear}. All rights reserved.
@@ -12,7 +12,7 @@ const Footer = () => {
       </div>
 
       <div className="container mx-auto flex items-center justify-center">
-        <p className="text-sm text-center">v0.0.12</p>
+        <p className="text-sm text-center">v0.0.13</p>
       </div>
     </footer>
   )
