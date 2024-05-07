5712b34d3217a696f748089918f7cce7e69b6ced
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2024-05-07 21:26:00 +1200

    Bump

--- iot-frontend/src/components/Footer.tsx
+++ iot-frontend/src/components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
       </div>
 
       <div className="container mx-auto flex items-center justify-center">
-        <p className="text-sm text-center">v0.1.0</p>
+        <p className="text-sm text-center">v0.1.1</p>
       </div>
     </footer>
   );
