00b31f98577ec092f97a413637f6c884366cbc19
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2024-05-08 14:50:21 +1200

    Fix alt image error

--- iot-frontend/src/components/AuthCheckAndAction.tsx
+++ iot-frontend/src/components/AuthCheckAndAction.tsx
@@ -1,15 +1,15 @@
-import React, { useEffect } from 'react'
-import { useSession } from 'next-auth/react'
-import { DynamicMetricData } from '@/types'
-import { showToast } from '@/components/Toast'
-import LoadingIndicator from '@/components/LoadingIndicator'
+import React, { useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { DynamicMetricData } from '@/types';
+// import { showToast } from '@/components/Toast'
+import LoadingIndicator from '@/components/LoadingIndicator';
 
 interface AuthCheckAndActionProps {
-  children: React.ReactNode
-  data: DynamicMetricData[]
-  loading: boolean
-  hasFetchedData: boolean
-  fetchDataAndUpdate: () => void
+  children: React.ReactNode;
+  data: DynamicMetricData[];
+  loading: boolean;
+  hasFetchedData: boolean;
+  fetchDataAndUpdate: () => void;
 }
 
 export function AuthCheckAndAction({
@@ -18,20 +18,20 @@ export function AuthCheckAndAction({
   hasFetchedData,
   fetchDataAndUpdate,
 }: AuthCheckAndActionProps) {
-  const { status } = useSession()
+  const { status } = useSession();
 
   useEffect(() => {
     // Only proceed if the user is authenticated
     if (status === 'authenticated' && !hasFetchedData) {
-      fetchDataAndUpdate()
+      fetchDataAndUpdate();
     } else if (status == 'unauthenticated') {
-      showToast({ message: 'User not authenticated', type: 'warning' })
+      // showToast({ message: 'User not authenticated', type: 'warning' })
     }
-  }, [status, hasFetchedData, fetchDataAndUpdate])
+  }, [status, hasFetchedData, fetchDataAndUpdate]);
 
   return (
     <>
       {loading ? <LoadingIndicator size={50} color="blue" message="Loading data..." /> : children}
     </>
-  )
+  );
 }
