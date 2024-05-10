42c431a395165f6c6b0985a2480120d31571e813
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2024-05-11 01:31:27 +1200

    Add Grid

--- iot-frontend/src/pages/landing-page/index.tsx
+++ iot-frontend/src/pages/landing-page/index.tsx
@@ -1,12 +1,48 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import AdroitImage from '@/public/assets/img/adroit_frontendblur.png';
-import AutLoge from '@/public/assets/img/AUT_Logo_New.jpg';
-import styles from '@/pages/landing-page/style.module.css';
+import Link from "next/link";
+import Image from "next/image";
+import AdroitImage from "@/public/assets/img/adroit_frontendblur.png";
+import AutLoge from "@/public/assets/img/AUT_Logo_New.jpg";
+import styles from "@/pages/landing-page/style.module.css";
 
 export default function Hero() {
   return (
     <section className="relative">
+      {/* The Grid */}
+      <div className="relative z-10">
+        <div className="absolute inset-x-0 top-1/2 -z-10 flex -translate-y-1/2 justify-center overflow-hidden [mask-image:radial-gradient(50%_45%_at_50%_55%,white,transparent)]">
+          <svg
+            className="h-[60rem] w-[100rem] flex-none stroke-blue-600 opacity-20"
+            aria-hidden="true"
+          >
+            <defs>
+              <pattern
+                id="e9033f3e-f665-41a6-84ef-756f6778e6fe"
+                width="200"
+                height="200"
+                x="50%"
+                y="50%"
+                patternUnits="userSpaceOnUse"
+                patternTransform="translate(-100 0)"
+              >
+                <path d="M.5 200V.5H200" fill="none"></path>
+              </pattern>
+            </defs>
+            <svg x="50%" y="50%" className="overflow-visible fill-blue-50">
+              <path
+                d="M-300 0h201v201h-201Z M300 200h201v201h-201Z"
+                stroke-width="0"
+              ></path>
+            </svg>
+            <rect
+              width="100%"
+              height="100%"
+              stroke-width="0"
+              fill="url(#e9033f3e-f665-41a6-84ef-756f6778e6fe)"
+            ></rect>
+          </svg>
+        </div>
+      </div>
+
       {/* Illustration behind hero content */}
       <div
         className="absolute md:left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1"
@@ -27,34 +63,14 @@ export default function Hero() {
               y2="100%"
               id="illustration-01"
             >
-              <stop
-                stopColor="#FFF"
-                offset="0%"
-              />
-              <stop
-                stopColor="#EAEAEA"
-                offset="77.402%"
-              />
-              <stop
-                stopColor="#DFDFDF"
-                offset="100%"
-              />
+              <stop stopColor="#FFF" offset="0%" />
+              <stop stopColor="#EAEAEA" offset="77.402%" />
+              <stop stopColor="#DFDFDF" offset="100%" />
             </linearGradient>
           </defs>
-          <g
-            fill="url(#illustration-01)"
-            fillRule="evenodd"
-          >
-            <circle
-              cx="1232"
-              cy="128"
-              r="128"
-            />
-            <circle
-              cx="155"
-              cy="443"
-              r="64"
-            />
+          <g fill="url(#illustration-01)" fillRule="evenodd">
+            <circle cx="1232" cy="128" r="128" />
+            <circle cx="155" cy="443" r="64" />
           </g>
         </svg>
       </div>
@@ -103,7 +119,7 @@ export default function Hero() {
               className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
               data-aos="zoom-y-out"
             >
-              Adroit{' '}
+              Adroit{" "}
               <span className="bg-clip-text text-transparent bg-gradient-to-b from-cyan-500 to-blue-500">
                 Frontend Manager
               </span>
@@ -114,7 +130,7 @@ export default function Hero() {
                 src={AutLoge}
                 width={250}
                 height={250}
-                style={{ width: '150px', height: 'auto' }}
+                style={{ width: "150px", height: "auto" }}
                 priority={true}
                 className="flex flex-col items-center justify-center"
               />
@@ -159,19 +175,22 @@ export default function Hero() {
             <div className="flex flex-col list-disc items-start gap-4 text-xl sm:text-2xl md:text-3xl leading-tighter tracking-tighter px-4 sm:px-8">
               <div className="flex flex-col gap-1">
                 <h2>Realtime reporting of offline devices</h2>
-                <p className="text-lg md:text-xl font-light">Using AWS S3, Lambda, API Gateway</p>
+                <p className="text-lg md:text-xl font-light">
+                  Using AWS S3, Lambda, API Gateway
+                </p>
               </div>
               <div className="flex flex-col gap-1">
                 <h2>Email integration for reporting devices</h2>
                 <p className="text-lg md:text-xl font-light">
-                  Fire off emails straight from the app, with one-click CC and BCC capabilities
+                  Fire off emails straight from the app, with one-click CC and
+                  BCC capabilities
                 </p>
               </div>
               <div className="flex flex-col gap-1">
                 <h2>Statistics monitoring</h2>
                 <p className="text-lg md:text-xl font-light">
-                  Gain deep insights into every device's performance through data visualizations and
-                  formatted text reports
+                  Gain deep insights into every device's performance through
+                  data visualizations and formatted text reports
                 </p>
               </div>
             </div>
@@ -183,10 +202,13 @@ export default function Hero() {
               Additional information
             </h1>
             <p className="px-4 sm:px-8 md:w-3/4 lg:w-1/2">
-              The project was initially conceived and undertaken as a research and development
-              endeavor by students at
-              <span className="font-bold"> Auckland University of Technology </span> during the
-              academic year spanning August 2023 to July 2024.
+              The project was initially conceived and undertaken as a research
+              and development endeavor by students at
+              <span className="font-bold">
+                {" "}
+                Auckland University of Technology{" "}
+              </span>{" "}
+              during the academic year spanning August 2023 to July 2024.
             </p>
             <div className="flex flex-wrap justify-center gap-1 px-4 sm:px-8">
               <h2 className="font-bold">Credits:</h2>
