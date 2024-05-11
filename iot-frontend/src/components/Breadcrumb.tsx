1c147be685ff408cfb8914d31715a3b5e35369c1
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2024-05-11 22:24:22 +1200

    Test Husky with linting error

--- iot-frontend/src/components/Breadcrumb.tsx
+++ iot-frontend/src/components/Breadcrumb.tsx
@@ -10,7 +10,7 @@ interface BreadcrumbProps {
   breadcrumbs: BreadcrumbItem[];
 }
 
-console.log("Test Husky 15");
+console.log("Test Husky 16");
 
 
 const Breadcrumb: React.FC<BreadcrumbProps> = ({ breadcrumbs }) => {
